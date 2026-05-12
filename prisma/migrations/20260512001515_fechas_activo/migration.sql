e9fff3a0638e0412df455329b2085e9b7acc4ac9
stivi007 <steeveg371@gmail.com>
2026-05-11 20:18:34 -0400

    chore: corrección de configuración inicial y tsconfig

---
+++ prisma/migrations/20260512001515_fechas_activo/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Usuario` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuario" ADD COLUMN     "activo" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUsuarioDto {}
+import { IsEnum, IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+enum Rol{
+    ADMIN = 'admin',
+    VENDEDOR = 'vendedor',
+}
+export class CreateUsuarioDto {
+
+    @IsString()
+    @IsNotEmpty()
+    nombre?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    apellido?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    correo?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(6)
+    password?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsEnum(Rol)
+    rol?: Rol;
+
+}
